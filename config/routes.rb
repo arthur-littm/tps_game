4d82e74a179a11dbd0f9af158122ae0ccd1e10b3
Lucien George <luciengeorge95@gmail.com>
2019-06-26 12:46:36 +0200

    adding user to game

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :games do
+  resources :users do
+    resources :games, only: [:new, :create]
+  end
+  resources :games, except: [:new, :create] do
     member do
       get 'invite'
     end
