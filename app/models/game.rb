78bfe1c3dc53019e04991d80d8285a0dd2692902
Lucien George <luciengeorge95@gmail.com>
2019-06-26 12:54:20 +0200

    migration, game and routes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
-  has_many :users
+  has_many :participants, foreign_key: :user_id, class_name: 'User'
   has_many :rounds
+  belongs_to :user
   validates :code, presence: true
 end
