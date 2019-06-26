25dd6458035a7c3f79a07c18398318793fd0bdc9
Lucien George <luciengeorge95@gmail.com>
2019-06-26 12:39:05 +0200

    games controller done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,50 @@
 class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :edit, :update, :destroy]
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new
+    @game.code = SecureRandom.hex(5)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(game_params)
+      redirect_to game_path(@game)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_path
+  end
+
+
+  private
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:code)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games do
+    resources :rounds do
+      member do
+        get 'result'
+      end
+    end
+  end
 end
