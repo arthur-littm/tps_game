44afeca194cb744316a107b3396a2b421809e212
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-27 11:46:01 +0200

    results

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,10 @@ class Game < ApplicationRecord
   validates :code, presence: true
 
   attr_accessor :user_name
+
+  def scores
+    users.map do |user|
+      [user.name, user.answers.map { |answer| answer.votes.size }.sum * 100]
+    end
+  end
 end
