07e6a5be6ba50ed65274872b8a848e61b9cea360
Lucien George <luciengeorge95@gmail.com>
2019-06-27 12:31:27 +0200

    bug fix

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,7 +4,7 @@ class RoundsController < ApplicationController
     @answer = Answer.new
     @user = User.find(session[:user_id])
     redirect_to result_game_round_path(@round.game, @round) if @round.game.users.size <= @round.answers.size
-    if Game.find_by(code: params[:game_id]).rounds.first == @round
+    if @round.game.rounds.first == @round
       Pusher.trigger('game-channel','start-game', {
         started: true
       })
