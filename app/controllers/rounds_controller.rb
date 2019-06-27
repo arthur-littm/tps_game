0f3d093f5e3ffa9595c142d598fefb60594e202c
Lucien George <luciengeorge95@gmail.com>
2019-06-27 12:19:59 +0200

    only owner of game can start game and all users are redirected when he does

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,6 +4,11 @@ class RoundsController < ApplicationController
     @answer = Answer.new
     @user = User.find(session[:user_id])
     redirect_to result_game_round_path(@round.game, @round) if @round.game.users.size <= @round.answers.size
+    if Game.find_by(code: params[:game_id]).rounds.first == @round
+      Pusher.trigger('game-channel','start-game', {
+        started: true
+      })
+    end
   end
 
   def result
