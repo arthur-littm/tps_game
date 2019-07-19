12ffaf51635f2b33f1dc0a7539fd3a4d779bb1e5
Lucien George <luciengeorge95@gmail.com>
2019-07-19 15:01:21 +0100

    fixed loop

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,7 +4,7 @@ class RoundsController < ApplicationController
     @answer = Answer.new
     @user = User.find(session[:user_id])
     redirect_to result_game_round_path(@round.game, @round) if @round.game.users.size <= @round.answers.size
-    if @round.game.rounds.first == @round
+    if @round.game.rounds.first == @round && @user.id == @round.game.user.id
       Pusher.trigger('game-channel','start-game', {
         started: true
       })
